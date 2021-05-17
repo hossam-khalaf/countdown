4348f661ac99b95f0b1f78dba164a96938cb5e44
hossam-khalaf <hossamhassan303089@gmail.com>
2021-05-17 10:12:58 +0200

    functionalty

---
+++ app.js
@@ -0,0 +1,29 @@
+const countdown = () => {
+	const countDate = new Date('May 30 , 2021 00:00:00').getTime()
+
+	const now = new Date().getTime()
+
+	const gap = countDate - now
+
+	// console.log(countDate, now, gap)
+
+	const sec = 1000
+	const min = sec * 60
+	const hour = min * 60
+	const day = hour * 24
+
+	const textDay = Math.floor(gap / day)
+	const textHour = Math.floor((gap % day) / hour)
+	const textMinute = Math.floor((gap % hour) / min)
+	const textSecond = Math.floor((gap % min) / sec)
+
+	// inject to DOM
+	document.querySelector('.day').innerText = textDay
+	document.querySelector('.hour').innerText = textHour
+	document.querySelector('.minute').innerText = textMinute
+	document.querySelector('.second').innerText = textSecond
+}
+
+setInterval(() => {
+	countdown()
+}, 1000)
